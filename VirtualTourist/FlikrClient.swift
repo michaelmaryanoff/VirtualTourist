493cacf3e75a2c4c993e8224d180b3852158c4e6
Michael Maryanoff <mmaryanoff@gmail.com>
2019-07-30 12:10:51 -0500

    Created a struct for holding URL constants

--- VirtualTourist/FlikrClient.swift
+++ VirtualTourist/FlikrClient.swift
@@ -10,17 +10,17 @@ import Foundation
 
 class FlikrClient {
     
-    let baseURLString = "https://www.flickr.com/services/rest/"
-    let apiKey = "api_key=cfcda78c06f98952812fc893f4c92f24"
-    let method = "method=flickr.photos.search"
-    let radius = "radius=10"
+    
     
     struct RequestConstants {
-        
+        static let baseURLString = "https://www.flickr.com/services/rest/"
+        static let apiKey = "api_key=cfcda78c06f98952812fc893f4c92f24"
+        static let method = "method=flickr.photos.search"
+        static let radius = "radius=10"
     }
     
     func requestPhotos(lat: Double, long: Double, completion: @escaping(Bool, Error?) -> Void) {
-        var url = baseURLString + "?" + method + "&" + apiKey + "&" + "lat=\(lat)" + "&" + "lon=\(long)" + "&" + radius + "&format=json" + "&nosjoncallback=1"
+        var url = RequestConstants.baseURLString + "?" + RequestConstants.method + "&" + RequestConstants.apiKey + "&" + "lat=\(lat)" + "&" + "lon=\(long)" + "&" + RequestConstants.radius + "&format=json" + "&nosjoncallback=1"
         var request = URLRequest(url: URL(string: url)!)
         
         let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
